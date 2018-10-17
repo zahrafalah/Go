63785e1b03b597c05072ffbbd9a0b6e1e305d262
zahra falah <sepidehfallah2@gmail.com>
2018-10-16 17:14:41 -0700

    functions and return types

--- Card/main.go
+++ Card/main.go
@@ -5,6 +5,11 @@ import "fmt"
 func main() {
 	//var Card string = "Ace of spring"
 	//Or
-	Card := "Ace of spring"
+	Card := newCard
 	fmt.Println(Card)
+}
+
+// a return function, when executed, it will return a value of type 'string'
+func newCard() string {
+	return "Ace of spring"
 }
